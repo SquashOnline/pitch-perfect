72e4de7832a3b794a2f0731fac028156e63d162d
SquashOnline <squash@squashonline.ca>
2015-04-27 23:36:32 -0700

    Committed by Udacity

---
+++ PitchPerfect/PitchPerfect/RecordedAudio.swift
@@ -0,0 +1,16 @@
+//
+//  RecordedAudio.swift
+//  PitchPerfect
+//
+//  Created by Benjamin Uliana on 2015-04-26.
+//  Copyright (c) 2015 SquashOnline. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject {
+    
+    var filePathURL: NSURL! = nil
+    var title: String! = nil
+    
+}
